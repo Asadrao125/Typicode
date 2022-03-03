308fd2c81c535bf75b304403d796e80f5cb113e3
asad.muqeem <raoasad125@gmail.com>
2022-03-03 13:15:48 +0500

    Initial Commit

--- app/src/main/java/com/technado/typicode/adapters/PostAdapter.kt
+++ app/src/main/java/com/technado/typicode/adapters/PostAdapter.kt
@@ -25,7 +25,6 @@ class PostAdapter(var context: Context, var list: List<PostModel>) :
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         binding?.tvTitle?.text = list[position].body
-
         binding?.tvComments?.setOnClickListener(View.OnClickListener {
 
         })
